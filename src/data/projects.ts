7db07f9b5690afee647ed26335987156626e0b80
vikashsingh02 <vikas2122panwar@gmail.com>
2026-06-17 13:36:33 +0530

    FixS

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,5 @@
 import { type ProjectCardProps } from "@/components/projects/project-card";
 import { type ProjectShowcaseListItem } from "@/components/projects/project-showcase-list";
-import { siteMetadata } from "@/data/siteMetaData.mjs";
 
 export const PROJECT_SHOWCASE: ProjectShowcaseListItem[] = [
   {
